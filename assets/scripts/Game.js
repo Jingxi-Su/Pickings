8947152d7222d3f7ae339e9559880563a2aea889
Jingxi <jingxi.su@shopee.com>
2022-07-29 15:26:32 +0800

    在随机位置生成星星

--- assets/scripts/Game.js
+++ assets/scripts/Game.js
@@ -32,13 +32,32 @@ cc.Class({
     }
   },
 
-  // LIFE-CYCLE CALLBACKS:
-
-  // onLoad () {},
-
-  start () {
-
+  onLoad: function () {
+    //获取地平面的y轴坐标
+    this.groundY = this.ground.y + this.ground.height / 2;
+    //生成新的star
+    this.spawnNewStar();
   },
+
+  spawnNewStar: function () {
+    //使用给定的模版在场景中生成一个新节点。instantiate() 用于克隆指定的任意类型的对象，或者从 Prefab 实例化出新节点，返回值为 Node 或者 Object
+    var newStar = cc.instantiate(this.starPrefab)
+    //将新增的节点添加到Canvas节点下。addChild() 用于在节点下添加一个新的子节点，所以新节点在场景中的显示效果在该节点之上
+    this.node.addChild(newStar)
+    //给星星设置随机位置，Node 下的 setPosition() 用于设置节点在父节点坐标系中的位置
+    newStar.setPosition(this.getNewStarPosition())
+  },
+
+  getNewStarPosition: function () {
+    var randX = 0;
+    //根据地平面位置和主角跳跃高度，随机得到一个星星的y坐标
+    var randY = this.groundY + Math.random() * this.player.getComponent('Player').jumpHeight + 50;
+    //根据屏幕宽度，随机得到一个星星的x坐标（原点在屏幕中心，所以需要/2）
+    var maxX = this.node.width / 2;
+    randX = (Math.random() - 0.5) * 2 * maxX;
+    //返回星星坐标，setPosition可以-1.传入两个数值 x 和 y；2.传入 cc.v2(x, y) 或 cc.v3(x, y, z)（类型为 cc.Vec2 或 cc.Vec3 的对象）
+    return cc.v2(randX, randY);
+  }
 
   // update (dt) {},
 });
