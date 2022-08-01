e665d3b7a44c5992cef824e2bb54e67f261d4a03
Jingxi <jingxi.su@shopee.com>
2022-08-01 16:19:55 +0800

    Star js to ts

--- assets/scripts/Star - 001.ts
+++ assets/scripts/Star - 001.ts
@@ -1,28 +1,46 @@
-// Learn TypeScript:
-//  - https://docs.cocos.com/creator/manual/en/scripting/typescript.html
-// Learn Attribute:
-//  - https://docs.cocos.com/creator/manual/en/scripting/reference/attributes.html
-// Learn life-cycle callbacks:
-//  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
 
-const {ccclass, property} = cc._decorator;
+const { ccclass, property } = cc._decorator;
 
 @ccclass
 export default class NewClass extends cc.Component {
 
-    @property(cc.Label)
-    label: cc.Label = null;
+  @property
+  // 星星和主角之间的距离小于这个数值时，就会完成收集
+  pickRadius: number = 0
 
-    @property
-    text: string = 'hello';
+  game = null
 
-    // LIFE-CYCLE CALLBACKS:
+  getPlayerDistance() {
+    // 根据Player节点位置判断距离
+    const playerPos = this.game.player.getPosition()
+    // 根据两点位置计算两点之间的距离
+    const dist = this.node.position.sub(playerPos).mag()
+    return dist
+  }
 
-    // onLoad () {}
+  onPicked() {
+    // 当星星被收集时，调用Game脚本中的接口，生成一个新的星星
+    this.game.spawnNewStar()
+    // 调用Game脚本的得分方法
+    this.game.gainScore()
+    // 销毁当前星星，通过 node.destroy() 函数，可以销毁节点
+    this.node.destroy()
+  }
 
-    start () {
+  start() {
 
+  }
+
+  update(dt: number) {
+    // 每帧判断星星和主角之间的距离是否小于收集距离
+    if (this.getPlayerDistance() < this.pickRadius) {
+      this.onPicked()
+      return
     }
 
-    // update (dt) {}
+    // 根据Game脚本中的计时器更新星星的透明度
+    const opacityRatio = 1 - this.game.timer / this.game.starDuration
+    const minOpacity = 50
+    this.node.opacity = minOpacity + Math.floor(opacityRatio * (255 - minOpacity))
+  }
 }
